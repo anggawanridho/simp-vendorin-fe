b61210a22350cb9200b1b61fb400f8c6cd5b8c1f
Fannn03 <fann.fann771@gmail.com>
2023-11-23 14:22:13 +0700

    feat(navbar): slicing search button

--- src/components/DashboardNavbar.tsx
+++ src/components/DashboardNavbar.tsx
@@ -7,6 +7,16 @@ const DashboardNavbar = () => {
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
         </button>
+        <form action="" method="post" className="flex items-center">
+          <div>
+            <input type="text" name="search" placeholder="Search..." autoComplete="false" className="bg-slate-100 rounded-l-md px-3 py-2" />
+            <button type="submit" className="px-3 py-2 bg-blue-400 hover:bg-blue-500 rounded-r-lg float-right right-10">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 text-white h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+              </svg>
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   )
