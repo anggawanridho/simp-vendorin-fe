0595362714fbf1e2effb602c4c61843ce914184e
Fannn03 <fann.fann771@gmail.com>
2023-11-23 11:46:25 +0700

    chore: add custom color dashboard

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "custom-color-dashboard": 'rgb(18, 51, 98)'
+      }
     },
   },
   plugins: [],
